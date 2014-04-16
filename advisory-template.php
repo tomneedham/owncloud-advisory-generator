9993fea148dad38e2345e9163cb55e9bbd9e0cfe
Tom Needham <tom@owncloud.com>
2014-04-16 14:08:39 +0100

    Initial code for generating advisories

---
+++ advisory-template.php
@@ -0,0 +1,30 @@
+<div class="row page-content-header">
+	<div class="col-md-4">
+		<h1>Security Advisory</h1>
+		<a href="/security/advisories/feed" class="rss-button">RSS</a>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-3">
+		<?php get_template_part('advisories/single-side-list'); ?>
+	</div>
+	<div class="col-md-9">
+		<h2><<TITLE>> (<<IDENTIFIER>>)</h2>
+		<p><<DATE>></p>
+		<p>Risk level: <strong><<LEVEL>></strong></p>
+		<h3>Description</h3>
+		<p><<DESCRIPTION>>>></p>
+		<h3>Affected Software</h3>
+		<ul>
+			<<AFFECTEDVERSIONS>>
+		</ul>
+		<h3>Action Taken</h3>
+		<p><<ACTION>></p>
+		<h3>Acknowledgements</h3>
+		<p>The ownCloud team thanks the following people for their research and responsible disclosure of the above advisory:</p>
+		<ul>
+			<<ACKNOWLEDGMENTS>>
+		</ul>
+		</br>
+	</div>
+</div>
---
+++ gen.php
@@ -0,0 +1,126 @@
+<?php
+
+/**
+ *
+ * Automatically generates the Wordpress page PHP files for advisories
+ *
+ * @author Tom Needham
+ * @copyright 2014 Tom Needham tom@owncloud.com
+ *
+ * Handles the dynamic nature of the scurity advisories section on ownCloud.org
+ *
+ * Part 1: It generates the .php files used to produce the 'advisory' pages for each advisory
+ * Part 2: It recreates the lists and menus that list the advisories.
+ *
+ * Usage: php gen.php <ADVISORIES REPO DIR> <WWW REPO DIR>
+ * (example) 'php gen.php /home/tom/security-advisories /home/tom/www'
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// PART 1 - Create the advisory files
+
+// 1.1 - Read the json files
+$advisoriesdir = $argv[1];
+$wwwrepo = $argv[2];
+$advisoryfiles = array();
+$software = array('desktop', 'mobile', 'server');
+$risklevel = array(
+	1 => 'Low',
+	2 => 'Medium',
+	3 => 'High',
+	);
+$advisories = array();
+
+foreach($software as $type) {
+	$files = scandir($advisoriesdir.'/'.$type);
+	foreach($files as $file) {
+		if($file != '.' && $file != '..') {
+			$advisoryfiles[basename($file, '.json')] = $advisoriesdir . '/' . $type . '/' . $file;
+		}
+	}
+}
+
+foreach($advisoryfiles as $advisoryfile) {
+	// Read the file
+	$file = file_get_contents($advisoryfile);
+	$data = json_decode($file);
+	$advisories[basename($advisoryfile, '.json')] = $data;
+}
+
+// 1.2 - Generate the PHP files
+foreach($advisories as $identifier => $advisory) {
+	$output = $wwwrepo . '/advisories/' . $advisory->Software . '/' . $identifier;
+	$template = file_get_contents(__DIR__.'/advisory-template.php');
+	// Insert the data
+	$template = str_replace('<<TITLE>>', htmlentities($advisory->Title), $template);
+	$template = str_replace('<<IDENTIFIER>>', htmlentities($identifier), $template);
+	$template = str_replace('<<DATE>>', htmlentities(date('jS F o', $advisory->Timestamp)), $template);
+	$template = str_replace('<<LEVEL>>', htmlentities($risklevel[$advisory->Risk]), $template);
+	$template = str_replace('<<DESCRIPTION>>', htmlentities($advisory->Description), $template);
+	$affectedversions = '';
+	foreach($advisory->Affected as $affected) {
+		$operator = isset($affected->Operator) ? $affected->Operator.' ' : '';
+		$affectedversions .= '<li>ownCloud Server '.$operator.'<strong>'.$affected->Version.'</strong> ('.$affected->CVE.')</li>';
+	}
+	$template = str_replace('<<AFFECTEDVERSIONS>>', htmlentities($affectedversions), $template);
+	$template = str_replace('<<ACTION>>', htmlentities($advisory->ActionTaken), $template);
+	$acknowledgments = '';
+	foreach($advisory->Acknowledgment as $acknowledgment) {
+		$acknowledgments .= '<li>'.$acknowledgment->Name.' - '.$acknowledgment->Company.' ('.$acknowledgment->Mail.') - '.$acknowledgment->Reason.'</li>';
+	}
+	$template = str_replace('<<ACKNOWLEDGMENTS>>', htmlentities($acknowledgments), $template);
+	file_put_contents($wwwrepo . '/advisories/' . $identifier . '.php', $template);
+}
+
+// PART 2 - Generate the menus and lists
+
+// 2.1 - Sort the advisories into versions
+$verisons = array();
+foreach($software as $type) {
+	$versions[$type] = array();
+}
+
+foreach($advisories as $identifier => $advisory) {
+	foreach($advisory->Affected as $affected) {
+		if(!array_key_exists($affected->Version, $versions[$advisory->Software])) {
+			$versions[$advisory->Software][$affected->Version] = array();
+		}
+		$versions[$advisory->Software][$affected->Version][$identifier] = $advisory;
+	}
+}
+
+// 2.2 - Generate the main advisories page lists
+foreach($software as $type) {
+	 $data = '';
+	 foreach($versions[$type] as $version => $advisories) {
+	 	$data .= '<p>Version ' . $version . "</p>\n";
+	 	foreach($advisories as $identifier => $advisory) {
+	 		$data .= '<a href="/security/advisory?id='.$identifier.'">'.$advisory->Title.'</a><br>'."\n";
+	 	}
+	 	$data .= '<br><br>'."\n";
+	 }
+	 file_put_contents($wwwrepo . '/advisories/' . $type . '-list-part.php', $data);
+}
+
+// 2.3 - Generate the lists at the side of the advisories
+$data = '';
+foreach($software as $type) {
+	$data .= '<p>ownCloud '.ucwords($type).'</p>'."\n";
+	// Add 5 latest of each TODO
+}
+file_put_contents($wwwrepo . '/advisories/advisory-side.php', $data);
+
+?>
